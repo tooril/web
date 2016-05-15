3dd40a2252e1a6e9ce4c34a48cb4febfaa21a16a
tooril <alexey_tokarev@ghost17.ru>
2016-05-15 21:14:16 +0000

    mla

--- hello.py
+++ hello.py
@@ -1,12 +1,17 @@
+import urlparse
 def app(environ, start_response):
 #	for key, value in environ.items():
 #		print (key, value)
-	print (environ.get("QUERY_STRING"))
-	data = "Hello, World!\n"
+	q = environ.get("QUERY_STRING")
+#	print (q)
+	#data = urlparse.parse_qs(q)
+	data = ['x=1\r\n','x=2\r\n','y=3\r\n','y=\r\n']
+#	for k,v in data.items():
+#		print(k,v)
 	status = "200 OK"
 	headers = [
 		('Content-Type','text/plain'),
 		('Content-Length', str(len(data)))
 	]
 	start_response(status,headers)
-	return iter([data])
+	return [data]
